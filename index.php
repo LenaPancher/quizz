0123e1a333007deb7a8c9f906c5bd8179ceb685f
Léna <lenapancher@gmail.com>
2021-02-28 01:16:25 +0100

    page index.php faite, design

--- index.php
+++ index.php
@@ -2,24 +2,34 @@
 include('navBar.php')
 ?>
 
-<body class="body">
-
-  <div class="containerAccueil">
-      <div class="containerImgAccueil w-30 p-3">
-          <img src="img/foret.jpg" alt="foret">
+<body class="bodyIndex">
+  <div class="container-fluid p-0">
+    <div class="row d-flex rowIndex">
+      <div class="col-4">
+        <img src="img/foret.jpg" alt="foret" class="w-100">
       </div>
-      <div class="containerTexteAcceuil">
-          <div>
-                <h1>TEST</h1>
+      <div class="col-8 mt-5">
+        <div class="row mt-5">
+          <div class="col-6 m-auto">
+            <span class="display-2">Testez</span>
           </div>
-          <div>
-            <!--
-            <a href="#" class="button">C'est parti !</a>
-            -->
+        </div>
+        <div class="row">
+          <div class="col-2 text-right m-auto">
+            <span class="display-2">vos</span>
           </div>
+        </div>
+        <div class="row">
+          <div class="col-7 m-auto">
+            <span class="display-2">connaissances</span>
+          </div>
+        </div>
+        <div class="text-center mt-5">
+          <a href="#" class="button h4">C'est parti !</a>
+        </div>
       </div>
     </div>
+  </div>
 
-    <script type="text/javascript" src="script.js"></script>
-
+  <script type="text/javascript" src="script.js"></script>
 </body>
--- navBar.php
+++ navBar.php
@@ -10,40 +10,42 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark navbar-default fixed-top">
-        <a href="index.php" class="mr-2 ml-5">
-            <img src="img/logo.png" alt="logo" height="60px" width="60px" />
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark navbar-default fixed-top">
+            <a href="index.php" class="mr-2 ml-5">
+                <img src="img/logo.png" alt="logo" height="60px" width="60px" />
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse mt-2 mb-2 justify-content-end" id="navbarSupportedContent">
-            <ul class="navbar-nav ">
-                <li class="nav-item active mr-2 ml-2">
-                    <a class="nav-link" href="index.php">Accueil</a>
-                </li>
-                <li class="nav-item dropdown mr-2 ml-2">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Quizz
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Tous les thèmes</a>
-                        <a class="dropdown-item" href="#">Test</a>
-                        <a class="dropdown-item" href="#">Test</a>
-                        <a class="dropdown-item" href="#">Test</a>
-                        <a class="dropdown-item" href="#">Test</a>
-                    </div>
-                </li>
-                <li class="nav-item mr-2 ml-2">
-                    <a class="nav-link" href="#">A Propos</a>
-                </li>
-                <li class="nav-item  mr-2 ml-2">
-                    <a class="nav-link" href="connexion.php">Se connecter</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+            <div class="collapse navbar-collapse mt-2 mb-2 justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item mr-2 ml-2"> 
+                        <a class="nav-link active" href="index.php">Accueil</a>
+                    </li>
+                    <li class="nav-item dropdown mr-2 ml-2">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Quizz
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Tous les thèmes</a>
+                            <a class="dropdown-item" href="#">Test</a>
+                            <a class="dropdown-item" href="#">Test</a>
+                            <a class="dropdown-item" href="#">Test</a>
+                            <a class="dropdown-item" href="#">Test</a>
+                        </div>
+                    </li>
+                    <li class="nav-item mr-2 ml-2">
+                        <a class="nav-link" href="#">A Propos</a>
+                    </li>
+                    <li class="nav-item  mr-2 ml-2">
+                        <a class="nav-link" href="connexion.php">Se connecter</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
